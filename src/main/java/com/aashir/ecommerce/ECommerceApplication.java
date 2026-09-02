54e4da0e0bd1556d5ff0b6e85c18d984378e5d14
Aashir Shaikh <aashirshaikhh09@gmail.com>
2026-09-02 16:04:00 +0530

    feat: add Redis caching

--- src/main/java/com/aashir/ecommerce/ECommerceApplication.java
+++ src/main/java/com/aashir/ecommerce/ECommerceApplication.java
@@ -2,11 +2,13 @@ package com.aashir.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class ECommerceApplication {
 
     public static void main(String[] args) {
